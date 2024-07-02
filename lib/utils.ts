34ebd495fc6b0c2e94f04706aa5ed1c5460af893
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-07-02 17:56:35 +0600

    Add timeAgo function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,40 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function timeAgo(dateString: string): string {
+  const date = new Date(dateString);
+  const now = new Date();
+
+  const seconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+  let interval = Math.floor(seconds / 31536000);
+
+  if (interval >= 1) {
+    return interval + " year" + (interval > 1 ? "s" : "") + " ago";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval >= 1) {
+    return interval + " month" + (interval > 1 ? "s" : "") + " ago";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval >= 1) {
+    return interval + " day" + (interval > 1 ? "s" : "") + " ago";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval >= 1) {
+    return interval + " hour" + (interval > 1 ? "s" : "") + " ago";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval >= 1) {
+    return interval + " min" + (interval > 1 ? "s" : "") + " ago";
+  }
+  return (
+    Math.floor(seconds) +
+    " second" +
+    (Math.floor(seconds) > 1 ? "s" : "") +
+    " ago"
+  );
 }
