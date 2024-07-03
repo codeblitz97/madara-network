c5e21a121173c7fea7cd70b08167f20abbee37c5
Mohtasim Alam Sohom <94981761+codeblitz97@users.noreply.github.com>
2024-07-03 16:31:37 +0600

    Add responsiveness to EpisodeCard

--- components/shared/EpisodeCard.tsx
+++ components/shared/EpisodeCard.tsx
@@ -28,9 +28,13 @@ export const EpisodeCard = ({
   const relativeTime = createdAt === "No date" ? "No Date" : timeAgo(createdAt);
 
   return (
-    <Link href={`/watch/${id}?episodeId=${episodeId}&subType=${sub}`}>
+    <Link
+      href={`/watch/${id}?episodeId=${encodeURIComponent(
+        episodeId
+      )}&subType=${sub}&number=${number}`}
+    >
       <motion.div
-        className="flex items-start gap-4 group rounded-lg p-4"
+        className="flex items-start gap-4 group rounded-lg p-4 sm:flex-row flex-col"
         whileHover={{ scale: 1.02 }}
         transition={{ type: "spring", stiffness: 300 }}
       >
@@ -41,20 +45,20 @@ export const EpisodeCard = ({
             alt={title}
             width={448}
             height={224}
-            className="aspect-video object-cover rounded-lg min-h-[124px] min-w-[248px] max-h-[124px] max-w-[248px]"
+            className="aspect-video object-cover rounded-lg sm:min-h-[124px] sm:min-w-[248px] sm:max-h-[124px] sm:max-w-[248px] w-full"
           />
           <span className="absolute z-50 rounded-lg bg-black/75 text-xs text-foreground bottom-2 right-2 px-2 py-1">
             EP {number}
           </span>
         </div>
         <div className="flex-1 grid gap-1">
-          <div className="flex items-center gap-2">
-            <h3 className="font-medium line-clamp-1">{title}</h3>
-            <span className="text-xs text-foreground-500">{relativeTime}</span>
-          </div>
+          <h3 className="font-medium line-clamp-1">{title}</h3>
           <p className="text-xs text-muted-foreground line-clamp-3">
             {overview}
           </p>
+          <span className="text-xs text-foreground-500">
+            Released {relativeTime}
+          </span>
         </div>
       </motion.div>
     </Link>
